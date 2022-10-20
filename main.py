9926299c0a8ea2af6b37099424c74c9203e4bd38
viktorsmirnov <smirn04@yandex.ru>
2022-10-20 11:25:19 +0300

    annotation lesson

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from random import randint
 
 def attack(char_name, char_class):
     if char_class == 'warrior':
-        return
-    return (f'{char_name} нанёс урон противнику равный {5 + randint(3, 5)}')
+        return (f'{char_name}'
+                f'нанёс урон противнику равный {5 + randint(3, 5)}')
     if char_class == 'mage':
-        return
-    return (f'{char_name} нанёс урон противнику равный {5 + randint(5, 10)}')
+        return (f'{char_name}'
+                f'нанёс урон противнику равный {5 + randint(5, 10)}')
     if char_class == 'healer':
-        return
-    return (f'{char_name} нанёс урон противнику равный {5 + randint(-3, -1)}')
+        return (f'{char_name}'
+                f'нанёс урон противнику равный {5 + randint(-3, -1)}')
 
 
 def defence(char_name, char_class):
